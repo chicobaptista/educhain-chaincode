5951dfc3ce8108bc9f3697829b07052a2fc675c9
Francisco Llamosas Baptista <f.baptista@ecotrace.info>
2021-03-28 21:20:49 -0300

    test: tests for read and create certificate

--- src/certificate/certificate-contract.spec.ts
+++ src/certificate/certificate-contract.spec.ts
@@ -11,7 +11,7 @@ import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import winston = require('winston')
 import { User } from '../user/user'
-import { CertificateContract } from './certificate-contract'
+import { CertificateContract } from '../index'
 import { Certificate } from './certificate'
 import { Course } from '../course/course'
 
@@ -67,9 +67,68 @@ describe('CertificateContract', () => {
     ctx.stub.getState
       .withArgs(certificate.id)
       .resolves(Buffer.from(JSON.stringify(certificate)))
+    ctx.stub.getState
+      .withArgs(student.id)
+      .resolves(Buffer.from(JSON.stringify(student)))
+    ctx.stub.getState
+      .withArgs(course.id)
+      .resolves(Buffer.from(JSON.stringify(course)))
   })
 
-  describe('#certificateExists', () => {})
+  describe('#certificateExists', () => {
+    it('should return true for a certificate', async () => {
+      await contract.certificateExists(
+        ctx,
+        '8fc68828-cd82-4554-a4ab-880c2077748c'
+      ).should.eventually.be.true
+    })
+
+    it('should return false for a certificate that does not exist', async () => {
+      await contract.certificateExists(
+        ctx,
+        'd9ff5a21-3f82-4c97-bf82-1037f33fba78'
+      ).should.eventually.be.false
+    })
+  })
+  describe('#createCertificate', () => {
+    const course: Course = {
+      id: 'f47d68f7-0883-4989-9757-39b32e5389ac',
+      name: 'My Course',
+      duration: 2,
+      instructor: 'b3bb80d9-799e-49e8-ab0f-841ffd8be8bc',
+      students: []
+    }
+    const certificate: Certificate = {
+      id: '8fc68828-cd82-4554-a4ab-880c2077748c',
+      studentId: 'b3bb80d9-799e-49e8-ab0f-841ffd8be8bc',
+      completionDate: new Date(),
+      duration: 2,
+      courseId: 'f47d68f7-0883-4989-9757-39b32e5389ac',
+      instructorId: 'b3bb80d9-799e-49e8-ab0f-841ffd8be8bc'
+    }
+    const student: User = {
+      id: 'b3bb80d9-799e-49e8-ab0f-841ffd8be8bc',
+      name: 'John Doe',
+      email: 'john.doe@email.com',
+      certificates: [],
+      publicKey:
+        'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJS+Q2rOf/itOXOfk2CA/tq7nIgKN197HafHsd3BShCzqPRkqCYuoZvPzMyyOnJNQWz2w6HeUrZCAGE6xKa1jqECAwEAAQ=='
+    }
+    it('should create a certificate', async () => {
+      const newCertificate = {
+        ...certificate,
+        id: 'fdfb7a6f-6d6f-459c-b968-8c89d6afbb74'
+      }
+      await contract.createCertificate(ctx, newCertificate)
+      ctx.stub.putState.should.have.been.calledOnceWithExactly(
+        newCertificate.id,
+        Buffer.from(JSON.stringify(newCertificate))
+      )
+    })
+    it('should throw an error for a certificate that already exists', async () => {})
+    it('should throw an error for a course that does not exist', async () => {})
+    it('should throw an error for an instructor that does not exist', async () => {})
+    it('should throw an error for a student that does not exist', async () => {})
+  })
   describe('#readCertificate', () => {})
-  describe('#createCertificate', () => {})
 })
